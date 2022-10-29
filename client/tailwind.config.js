b0f921f40a7626c46b8644c83bca7c6f2c486010
Winnard Arthur <winnardarthur7@gmail.com>
2022-10-29 17:49:12 +0000

    feat(YGF): add socket.io and tailwindcss

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       backgroundImage: {
         "background": "url('../public/images/protruding-squares.png')",
-      }
+      },
     },
   },
   plugins: [
